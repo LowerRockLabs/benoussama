7d8c6e3b9a711de50a2374607ab2948ddb554d3d
Oussama Bendriss <oussama.bendriss@outlook.com>
2023-02-24 00:56:00 +0100

    adding cart button

--- app/Http/Livewire/LinkTable.php
+++ app/Http/Livewire/LinkTable.php
@@ -5,20 +5,47 @@ namespace App\Http\Livewire;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Link;
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\Views\Columns\BooleanColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
 use Rappasoft\LaravelLivewireTables\Views\Filters\DateFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\MultiSelectDropdownFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\MultiSelectFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\NumberFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\TextFilter;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
 
 class LinkTable extends DataTableComponent
 {
+
+    use LivewireAlert;
+
     protected $model = Link::class;
 
     public array $arrayOfCountries = [];
+
+    protected $listeners = [
+        'createOrder'
+    ];
+
+    public function createOrder(Link $link)
+    {
+        if (!$link->is_on_order) {
+            $this->alert('error', "You already purchased this link!");
+            return;
+        }
+
+        $order = Order::create([
+            'user_id' => auth()->id(),
+            'link_id' => $link->id,
+            'price' =>  $link->price + 150,
+        ]);
+
+        $this->alert('success', "Order {$order->id} created Successfully!");
+    }
 
     public function mount()
     {
@@ -45,9 +72,26 @@ class LinkTable extends DataTableComponent
     public function columns(): array
     {
         return [
+            Column::make("Cart")
+                ->label(
+                    function ($row, Column $column) {
+                        if ($row->orders->where('user_id', auth()->id())->isEmpty()) {
+                            return '<svg wire:click="$emit(\'createOrder\', {link:' . $row->id . '})" class="w-5 h-5 text-blue-600 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                          </svg>';
+                        } else {
+                            return '<svg class="w-5 h-5 text-gray-600 cursor-not-allowed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                          </svg>';
+                        }
+                    }
+                )
+                ->html(),
+
             Column::make("#", "id")
                 ->sortable()
                 ->searchable(),
+
             Column::make("Site", "site")
                 ->sortable()
                 ->searchable(),
@@ -65,6 +109,7 @@ class LinkTable extends DataTableComponent
                 ->sortable(),
 
             Column::make("Purchased Before")
+                ->sortable()
                 ->eagerLoadRelations()
                 ->label(
                     function ($row, Column $column) {
